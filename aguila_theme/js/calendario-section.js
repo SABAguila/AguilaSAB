a3395146dd4f9b353dd53c28fe87a52522c780d9
unknown <gaguilar@gaguilar.studiocom.ad>
2015-04-27 10:26:30 -0500

    added complete files Aguila

---
+++ aguila_theme/js/calendario-section.js
@@ -0,0 +1,217 @@
+/*global jQuery2 */
+(function ($, jq) {
+    var eventos, fechas, dias, colorEvento, fechaEvento, overlays, dia, evento, banner;
+        
+    function checkOverlayPosition($) {
+        $(".evento-calendario-overlay").each(function(i,overlay){
+            overlay = $(overlay);
+            monthview = overlay.closest(".monthview");
+            //reset
+            overlay.removeClass("forceup");
+            overlay.attr("style","");
+            overlay.find(".box-arrow").attr("style","");
+            //test
+            if(monthview.offset().left+monthview.width()/2<220) {
+                overlay.addClass("forceup");
+                if(overlay.offset().left<0) {
+                    if (overlay.css("left").indexOf("px")>-1) {
+                        overlay.css("left", parseInt(overlay.css("left")) - overlay.offset().left )
+                    }
+                }
+                var arrowx = monthview.width()/2 - parseInt(overlay.css("margin-left")) - overlay.position().left;
+                overlay.find(".box-arrow").css("left",arrowx);
+
+            }
+            
+        });
+    }
+    
+    function transformarCal() {
+        $(".view-calendario-chicas-aguila").find(".calendar-calendar:not(.transformado)").transform({
+            process: function ($, calendar) {
+                calendar.addClass('transformado');
+                
+                fechas = calendar.find("tr.date-box");
+                dias = calendar.find("tr.single-day");
+                eventos = dias.find(".evento-calendario");
+                overlays = $('.evento-calendario-overlay');
+
+                // Add some Evento styles and markup modifications
+                eventos.each(function(i, el) {
+                    colorEvento = $(el).find(".color").text().toLowerCase();
+                    fechaEvento = $(el).closest("td").attr("data-date");
+                    dia = fechas.find("td[data-date*="+fechaEvento+"]");
+                    evento = dias.find("td[data-date*="+fechaEvento+"]");
+                    // Add color based on selection
+                    dia.addClass(colorEvento);
+                    evento.find('.inner').addClass(colorEvento);
+                    // Duplicate day on overlay
+                    evento.find('.evento-calendario-overlay .date').text(dia.find('.day').text());
+                    // Add custom banner as background
+                    banner = evento.find('.bg');
+                    if($(banner).length) {
+                        banner.closest('.banner').css('backgroundImage', 'url(' + banner.find('img').attr('src') + ')');
+                    }
+                });
+
+                // Reusing some variables
+                fechaEvento = "";
+                dia = "";
+                evento = "";
+                // Add click event to display Evento overlay
+                fechas.find('td').on('click touchend', function(event) {
+                    fechaEvento = $(this).attr('data-date');
+                    evento = dias.find("td[data-date*="+fechaEvento+"]");
+                    // Hide all overlays and show the correct overlay
+                    overlays.addClass("hidden");
+                    evento.find(".evento-calendario-overlay").removeClass("hidden");
+                    checkOverlayPosition($);
+                    // Deactivate selected state in Dias and Eventos and activate only for the correct
+                    fechas.find('td').removeClass('evento-selected');
+                    dias.find('td').removeClass('evento-selected');
+                    $(this).addClass('evento-selected');
+                    evento.addClass('evento-selected');
+                    event.preventDefault();
+                });
+                dias.find('td').on('click touchend', function(event) {
+                    fechaEvento = $(this).attr('data-date');
+                    dia = fechas.find("td[data-date*="+fechaEvento+"]");
+                    // Hide all overlays and show the correct overlay
+                    overlays.addClass("hidden");
+                    $(this).find(".evento-calendario-overlay").removeClass("hidden");
+                    checkOverlayPosition($);
+                    // Deactivate selected state in Dias and Eventos and activate only for the correct
+                    fechas.find('td').removeClass('evento-selected');
+                    dias.find('td').removeClass('evento-selected');
+                    $(this).addClass('evento-selected');
+                    dia.addClass('evento-selected');
+                    event.preventDefault();
+                });
+
+                var fecha = $(".date-heading h3").html().replace(/[A-Za-z\u00C0-\u017F]+, ([A-Za-z\u00C0-\u017F]+) [0-9]+, ([0-9]+)/gi, "$1, $2");
+                $(".date-heading h3").html(fecha).addClass("formatted");
+                
+                
+               $("td.single-day:not(.no-entry):not(.empty)").each(function(i,el){
+                    var evento = $("<div class='event-tobe'></div>");
+                    var partesFecha = $(el).attr("data-date").split("-");
+                    evento.attr("data-date", partesFecha[1]+"-"+partesFecha[2]+"-"+partesFecha[0]);
+                    evento.append($(el).find(".evento-calendario-overlay").clone().removeClass("hidden"));
+
+                    jQuery("#mobile-calendar .events").append(evento);
+                });
+
+                var partesFecha = $($("td.single-day")[10]).attr("data-date").split("-");
+                jQuery("#mobile-calendar").dzscalendar({ 
+                    settings_alwaysinclude6rows: "on",
+                    start_month: partesFecha[1],
+                    start_year: partesFecha[0]
+                 });
+
+                $('#mobile-calendar .week-day').click(function(e){
+                    if($(this).hasClass('hasEvent')){
+                        e.stopPropagation();
+                        var date = $(this).data('date');
+                        date = date.split('-');
+                        date = date[2]+'-'+n(date[0])+'-'+n(date[1]);
+                        var dateSelected = new Date(date);
+                        var numDay = $(this).find('.the-number').text();
+                        var nameDay = getNameDay(dateSelected);
+                        var nameEvent = $(this).find('h4.title').text();
+                        var currentMonth = $('.date-nav .formatted').text();
+                        currentMonth = currentMonth.split(',');
+                        var image = $('.banner-calendario-chicas-aguila .banner .banner-'+currentMonth[0].toLowerCase()+' img').attr('src');
+                        $('.popup-evento-mobile').css('background','url("'+image+'") no-repeat');
+                        popupEventMobile(numDay, nameDay, nameEvent);
+                        $('.popup-evento-mobile').css('display','block');
+                        return false;
+                    }
+                });
+
+                $(document).click(function(){
+                    $('.popup-evento-mobile').css('display','none');
+                });
+
+            }
+        });
+    }
+
+    function getNameDay(date){
+        var weekday = new Array(7);
+        weekday[0]=  "Domingo";
+        weekday[1] = "Lunes";
+        weekday[2] = "Martes";
+        weekday[3] = "Miércoles";
+        weekday[4] = "Jueves";
+        weekday[5] = "Viernes";
+        weekday[6] = "Sábado";
+
+        var n = weekday[date.getDay()+1]; 
+        return n;
+    }
+
+    function n(n){
+        return n > 9 ? "" + n: "0" + n;
+    }
+
+    function popupEventMobile(numDay, nameDay, nameEvent) {
+        $('.popup-evento-mobile .number').text('');
+        $('.popup-evento-mobile .day').text('');
+        $('.popup-evento-mobile .title').text('');
+
+        $('.popup-evento-mobile .number').text(numDay);
+        $('.popup-evento-mobile .day').html(nameDay);
+        $('.popup-evento-mobile .title').text(nameEvent);
+    }
+
+    function setTextCurrentDate() {
+        var currentDate = $(".view-calendario-chicas-aguila").find('td.today').data('date');
+        $(".view-calendario-chicas-aguila").find('tr.single-day').find("[data-date='" + currentDate + "']").find('.inner').html('<div class="hoy">hoy</div>');
+    }
+
+    function hideImagesMonth() {
+        if($(window).width() >= 600){
+            var currentMonth = $('.date-nav .formatted').text();
+            currentMonth = currentMonth.split(',');
+            var arrayMonthImage = $('.view-calendario-chicas-aguila .view-banner-calendario-chicas-aguila .banner ').find('div');
+            $.each(arrayMonthImage, function(index, value){
+                var monthBanner = $(this).attr('class').split('-');
+                monthBanner = monthBanner[1];
+                if(currentMonth[0].toLowerCase() == monthBanner.toLowerCase()){
+                    $(this).show();
+                }
+            });
+        }
+        else{
+            var arrayMonthImage = $('.view-calendario-chicas-aguila .view-banner-calendario-chicas-aguila .banner ').find('div');
+            $.each(arrayMonthImage, function(index, value){
+                $(this).hide();
+            });
+        }
+    }
+
+    function printRedSunday() {
+        var sundays = $('.view-calendario-chicas-aguila tbody .date-box td');
+        $.each(sundays, function(){
+            if($(this).attr('headers') == 'Domingo'){
+                $(this).find('.month.day').css('color','#FF0000');
+            }
+        });
+    }
+
+    $(window).resize(function(){console.info('asd');
+        hideImagesMonth();
+    });
+
+    $.initModule(".view-calendario-chicas-aguila", function () {
+        transformarCal();
+        hideImagesMonth();
+        setTextCurrentDate();
+        printRedSunday();
+        jq(document).ajaxComplete(function () {
+            setTimeout(transformarCal, 50);
+            setTimeout(hideImagesMonth, 50);
+            setTimeout(printRedSunday, 50);
+        });
+    });
+}(jQuery2, jQuery));
---
+++ aguila_theme/js/lista-ruta-lugares.js
@@ -0,0 +1,226 @@
+/*global jQuery2, jQuery, document */
+(function ($, jq) {
+
+    var map;
+    var markers = [];
+    var centerLatlng;
+
+    function transformar() {
+        // no se puede utilizar la referencia a la lista porque el nodo en el DOM cambia
+        $('.lista-lugares-fiesta').find('.view-content > .item-list > ul > li.views-row:not(.transformado)').transform({
+            process: function ($, el) {
+                var rating,
+                    ratingText;
+
+                el.addClass('transformado clearfix');
+
+                if (el.find('.favoritos.logged-in').length === 0) {
+                    el.find('.favoritos.hidden').removeClass('hidden');
+                }
+                if (el.find('.favoritos-hover.link')) {
+                    el.find('.favorites-hover.hidden').removeClass('hidden');
+                }
+
+                // clean the rating field
+                rating = el.find('.views-field-field-rating .clearfix');
+                ratingParent = rating.parent();
+                rating.remove();
+                ratingParent.prepend('<span>' + rating.text().replace('/5', '') + '</span>');
+                
+            }           
+        });
+        
+        var clase_tipo_evento = $('.view-lista-de-lugares-de-fiesta li .views-field-field-tipo-de-lugar .field-content').find('div');
+        $.each(clase_tipo_evento, function(index, value){
+            var clases = $(this).attr('class');
+            $(this).closest('li').addClass(clases);
+        });
+
+    }
+
+    function unsortTilesEvents(){        
+        var wrapList = $('.view-lista-de-lugares-de-fiesta .view-content .item-list ul');
+        var mobileCellPhone = (/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini|windows phone/i.test(navigator.userAgent));
+
+        if( wrapList.length > 0 && document.documentElement.clientWidth > 736 && !mobileCellPhone){            
+            var container = document.querySelector('.view-lista-de-lugares-de-fiesta .view-content .item-list ul');            
+            var msnry = new Masonry( container, {         
+                itemSelector: 'li'              
+            });
+            setTimeout(function(){ 
+                msnry.reloadItems();
+                msnry.layout();  
+            }, 700);            
+        }
+    }
+    
+    
+    function validGeolocation(value) {
+        return value !== undefined && value !== null && value !== '';
+    }
+    function centerMap() {
+        var latlngbounds = new google.maps.LatLngBounds();
+        $(markers).each(function(i,data) {
+           latlngbounds.extend(data.marker.getPosition());
+        });
+        centerLatlng = latlngbounds.getCenter();
+        map.setCenter(centerLatlng);
+        map.fitBounds(latlngbounds); 
+    }
+
+    function convertDate(date){
+        date = date.split(' ');
+        date = date[0].split('-');
+        var arrayMonths = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+        var newDate = date[2] + ' ' + arrayMonths[date[1]-1] + ' ' + date[0];
+        return newDate;
+    }
+    
+    function updateMapMarkers() {
+        
+        
+        mapOptions = {
+            zoom: 6,
+            styles: $.mapStyles.grayscale,
+            center: new google.maps.LatLng(4.6194477,-74.110567),
+            scrollwheel: false,
+        },
+        map = new google.maps.Map($('.mapa-lugar-detalle')[0], mapOptions);
+        $('.mapa-lugar-detalle').removeClass("hidden");
+        
+        var linksWithCoords = $('.lista-lugares-fiesta').find('.view-map');
+        linksWithCoords.each(function(i, node) {
+            node = $(node);
+            if (validGeolocation(node.data('latitude')) && validGeolocation(node.data('longitude'))) {
+                var position = new google.maps.LatLng(parseFloat(node.data('latitude')),parseFloat(node.data('longitude')));
+                var iconBase = '/sites/g/files/ogq1136/f/201504/';
+                var fechaFiesta = convertDate(node.data('fecha'));
+                var horarioFecha = node.data('horario');
+                var lugarFiesta = node.data('lugar');
+                var cuidadFiesta = node.data('ciudad');
+                var marker = new google.maps.Marker({
+                    position: position,
+                    title: node.data('placename'),
+                    icon: iconBase + 'markerMaps.png'
+                });
+                infowindow = new google.maps.InfoWindow({
+                    content: '<div class="lugar-info-window map-info-window"><img src="/sites/g/files/ogq1136/f/201504/CarnavalTooltipImg.png"><h1>' + node.data('placename') + '</h1><ul id="social"><li class="facebook"></li><li class="twitter"></li></ul><div class="information"><div class="fecha"><strong>Fecha:</strong> '+fechaFiesta+'</div><div class="horario"><strong>Hora:</strong> '+horarioFecha+'</div><div class="ciudad"><strong>Ciudad:</strong> '+cuidadFiesta+'</div><div class="lugar"><strong>Lugar:</strong> '+lugarFiesta+'</div></div></div>',
+                    maxWidth : 215
+                });
+                node.data("marker",marker);
+                node.data("position",position);
+                node.data("infowindow",infowindow);
+                markers.push({marker:marker, infowindow: infowindow});
+                node.bind("click", function(event ) {
+                    event.preventDefault();
+                    jQuery("body").scrollTo('.mapa-lugar-detalle',800);
+                    $(markers).each(function(j,alldata) { alldata.infowindow.close(); });
+                    var position = $(this).data("position");
+                    var marker = $(this).data("marker");
+                    map.panTo(position);
+                    map.setZoom(16);
+                    $(this).data("infowindow").open(map, marker);
+                });
+                
+                marker.setMap(map);
+
+                google.maps.event.addListener(infowindow, 'domready', function() {
+
+                    // Reference to the DIV which receives the contents of the infowindow using jQuery
+                    var iwOuter = $('.gm-style-iw');
+
+                    /* The DIV we want to change is above the .gm-style-iw DIV.
+                    * So, we use jQuery and create a iwBackground variable,
+                    * and took advantage of the existing reference to .gm-style-iw for the previous DIV with .prev().
+                    */
+                    var iwBackground = iwOuter.prev();
+
+                    // Remove the background shadow DIV
+                    iwBackground.children(':nth-child(2)').css({'display' : 'none'});
+
+                    // Remove the white background DIV
+                    iwBackground.children(':nth-child(4)').css({'display' : 'none'});
+
+                    // Moves the shadow of the arrow 76px to the left margin 
+                    iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'display: none !important;'});
+
+                    // Moves the arrow 76px to the left margin 
+                    iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'display: none !important;'});
+
+
+                    // Taking advantage of the already established reference to
+                    // div .gm-style-iw with iwOuter variable.
+                    // You must set a new variable iwCloseBtn.
+                    // Using the .next() method of JQuery you reference the following div to .gm-style-iw.
+                    // Is this div that groups the close button elements.
+                    var iwCloseBtn = iwOuter.next();
+
+                    // Apply the desired effect to the close button
+                    iwCloseBtn.css({
+                        opacity: '1', // by default the close button has an opacity of 0.7
+                        right: '38px',
+                        top: '3px', // button repositioning                        
+                        'border-radius': '13px', // circular effect
+                        'box-shadow': 'rgb(11, 93, 153) 0px 0px 5px' // 3D effect to highlight the button
+                        
+                    });
+                    iwCloseBtn.next().css({
+                        right: '15%',
+                        'width': '40px'
+                    });
+
+                    // The API automatically applies 0.7 opacity to the button after the mouseout event.
+                    // This function reverses this event to the desired value.
+                    iwCloseBtn.mouseout(function(){
+                        $(this).css({opacity: '1'});
+                    });
+
+                });
+                
+                google.maps.event.addListener(marker, 'click', function () {
+                    $(markers).each(function(j,alldata) { 
+                        if(alldata.marker == marker) {
+                            alldata.infowindow.open(map, marker);
+                        }else{
+                            alldata.infowindow.close(); 
+                        }
+                    });
+                });
+                
+            }else{
+                node.addClass("hidden");
+            }
+        });
+        if (markers.length>0) {
+            $('.view-map-all').bind("click",function(){
+                event.preventDefault();
+                map.setZoom(6);
+                $(markers).each(function(j,alldata) { alldata.infowindow.close(); });
+                map.panTo(centerLatlng);
+                jQuery("body").scrollTo('.mapa-lugar-detalle',800);
+
+            });
+            google.maps.event.addListener(map, 'click', function () {
+                map.setOptions({
+                    scrollwheel: true,
+                });
+            });
+            google.maps.event.addDomListener(window, "resize", function() {
+                centerMap();
+            });
+            centerMap();
+        }
+    }
+    $.initModule('.lista-lugares-fiesta', function () {
+        transformar();
+        unsortTilesEvents();
+        jq(document).ajaxComplete(function () {
+            setTimeout(transformar, 50);
+            unsortTilesEvents();
+        });
+        $.mapsLoaded.promise().then(function () {
+            updateMapMarkers();
+        });
+    });    
+
+}(jQuery2, jQuery));
