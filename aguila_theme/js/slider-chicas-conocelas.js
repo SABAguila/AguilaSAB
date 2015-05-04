fe803111d0b62556201e0aa789cdd56848107481
unknown <gaguilar@gaguilar.studiocom.ad>
2015-05-04 14:37:28 -0500

    updated script

--- aguila_theme/js/slider-chicas-conocelas.js
+++ aguila_theme/js/slider-chicas-conocelas.js
@@ -26,14 +26,7 @@
             variableWidth: true,
             responsive: [
                 {
-                  breakpoint: 1024,
-                  settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3
-                  }
-                },
-                {
-                  breakpoint: 768,
+                  breakpoint: 1200,
                   settings: {
                     slidesToShow: 3,
                     slidesToScroll: 3
@@ -69,3 +62,5 @@
         //Add down arrow below the banner  
     });
 }(jQuery2));
+
+
