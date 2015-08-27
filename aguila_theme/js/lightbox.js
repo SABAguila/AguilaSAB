63fdee156d6b94006fe969414ba75bd688b72e34
unknown <egalindo@egalindo.studiocom.ad>
2015-08-26 19:05:11 -0500

    fix share gallery

--- aguila_theme/js/lightbox.js
+++ aguila_theme/js/lightbox.js
@@ -13,7 +13,7 @@
         var NumImgHash = parseInt(location.hash.substring(8));
 
         setTimeout(function(){
-            console.log("next, prev counter watching parseInt");
+            console.log("next, prev counter watching");
             if(NumImgHash != ""){
                 $(".media-gallery-thumb>img").eq(NumImgHash - 1).trigger("click");
             }
@@ -25,21 +25,33 @@
         if($(lightboxElements).length) {
             // Repeatedly remove colorbox until it achieves
             colorboxKiller = setInterval(function(){
-                removeColorbox(); 
+                removeColorbox();
             }, 100);
 
             //Set the specific image selected so that the popUp library con be manipulated for sharing
             $('body').on('click touch', '.media-gallery-media-item-thumbnail', function(e) {
-                var indexImg = parseInt($(this).parent().index()) + 1;
-                console.log("Numner Image: "+indexImg);
+                var indexImg = $(this).parent().index() + 1;
+                var galleng = $(lightboxElements).length;
                 location.hash="galeria"+indexImg;
 
                 $.magnificPopup.instance.next = function() {
-                    location.hash="galeria"+indexImg + 1;
+                    if(indexImg == galleng){
+                        indexImg = 1;    
+                    }
+                    else{
+                        indexImg++;                        
+                    }
+                    location.hash="galeria"+indexImg;
                     $.magnificPopup.proto.next.call(this);
                 };
                 $.magnificPopup.instance.prev = function() {
-                     location.hash="galeria"+indexImg - 1;
+                    if(indexImg == 1){
+                        indexImg = galleng;    
+                    }
+                    else{
+                        indexImg--;
+                    }
+                     location.hash="galeria"+indexImg;
                     $.magnificPopup.proto.prev.call(this);
                 };
 
@@ -49,7 +61,8 @@
             $('body').on('click touch', '.media-gallery-social-share a', function(e) {
                 //var mediaUrl = $(this).parents('figure').eq(0).find('.mfp-img').attr('src') ;
                 var mediaUrl = location.href;
-                socialShare($(this).attr('class'), mediaUrl);
+                var imgurl = $('.media-gallery-social-share a').closest('figure').find('img').attr('src');
+                socialShare($(this).attr('class'), mediaUrl, imgurl);
                 e.preventDefault();
             });
 
@@ -69,7 +82,7 @@
         $(lightboxElements).magnificPopup({
             type: 'image',
             tClose: 'Cerrar (Esc)',
-            tLoading: 'Cargando...',  
+            tLoading: 'Cargando...',
                 callbacks: {
                     open: function() {
                         $('.mfp-prevent-close').insertBefore('.mfp-close');
@@ -99,11 +112,11 @@
         clearInterval(colorboxKiller);
     }
 
-    function socialShare(social, path) {
+    function socialShare(social, path, img) {
         switch(social) {
             case 'fb':
-            window.open( 'https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(path),
-                '', 'status=1,width=626,height=436,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no');
+            //window.open( 'https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(path),+'&p[images][0]=' + encodeURIComponent('http://aguilaosg042015.sabmiller.acsitefactory.com/sites/g/files/ogq1136/f/styles/media_gallery_large/public/201501/FOTO-CONTRAPORTADA-PARED.jpg'), 'status=1,width=626,height=436,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no');
+            window.open( 'https://www.facebook.com/dialog/feed?app_id=277460082360332&redirect_uri=http://www.cervezaaguila.com/closepopup&picture='+encodeURIComponent(img)+'&display=popup&link='+encodeURIComponent(path),'','status=1,width=626,height=436,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no');
             break;
             case 'twitter':
             window.open( 'http://twitter.com/share?url='+encodeURIComponent(path),
@@ -113,3 +126,4 @@
     }
 
 }(jQuery, jQuery2));
+
